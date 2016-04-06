8157d5d5de6338cbf7e29f3d1db44d094b4931dd
caseyfisch <caseyfischer@Caseys-MacBook-Pro.local>
2016-04-06 17:55:54 -0400

    Add data to questions and wire collection into questionList template

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,18 +1,16 @@
-var questionsData = [
-  {
+if (Questions.find().count() === 0) {
+  Questions.insert({
     title: "How is 05-499C Crowd Programming?",
     description: "I want to take a fun class with Professor Bigham!"
-  },
-  {
+  });
+
+  Questions.insert({
     title: "How is 15-112 with Kosbie?",
     description: "I have no programming experience."
-  },
-  {
+  });
+
+  Questions.insert({
     title: "What's the easiest CS Logic elective?",
     description: "Ideally less than 8 hours of work per week."
-  }
-];
-
-Template.questionsList.helpers({
-  questions: questionsData,
-});
+  });    
+}
