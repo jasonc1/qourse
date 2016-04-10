08a8512a68b1cf776e1fd01fee77fd40f3b857a1
caseyfisch <caseyfischer@Caseys-MBP.wv.cc.cmu.edu>
2016-04-09 21:45:22 -0400

    Edit course pages, items and routes

--- server/fixtures.js
+++ server/fixtures.js
@@ -20,4 +20,9 @@ if (Courses.find().count() === 0) {
     title: "15-112",
     description: "A technical introduction to the fundamentals of programming with an emphasis on producing clear, robust, and reasonably efficient code using top-down design, informal analysis, and effective testing and debugging. Starting from first principles, we will cover a large subset of the Python programming language, including its standard libraries and programming paradigms. We will also target numerous deployment scenarios, including standalone programs, shell scripts, and web-based applications. This course assumes no prior programming experience. Even so, it is a fast-paced and rigorous preparation for 15-122. Students seeking a more gentle introduction to computer science should consider first taking 15-110. NOTE: students must achieve a C or better in order to use this course to satisfy the pre-requisite for any subsequent Computer Science course."
   });
+
+  Courses.insert({
+    title: "05-499",
+    description: "A: Chinmay Kulkarni - Learning with Peers at Massive Scale D: Jeff Bigham and Jessica Hammer - Twitch Plays Games Design"
+  });
 }
