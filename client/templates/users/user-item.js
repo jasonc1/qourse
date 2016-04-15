53fe63aa614d0681a41c639b60bafc6d694a2d95
Casey Fischer <caseyfischer@Caseys-MBP.wv.cc.cmu.edu>
2016-04-15 12:11:15 -0400

    Link to user from question page, list all questions by user on profile

--- client/templates/users/user-item.js
+++ client/templates/users/user-item.js
@@ -3,4 +3,9 @@ Template.userItem.helpers({
     var userVar = FlowRouter.getParam("username");
     return Meteor.users.findOne({username: userVar});
   },
+
+  userQuestions: function() {
+    var userVar = FlowRouter.getParam("username");
+    return Questions.find({author: userVar});
+  }
 });
