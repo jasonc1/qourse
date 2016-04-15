b9da77f39157e3bd8e3ad307fcf42e18fcda3738
Casey Fischer <caseyfischer@Caseys-MBP.wv.cc.cmu.edu>
2016-04-15 11:51:55 -0400

    Render (VERY) basic profile page, link to profiles from questions

--- client/templates/users/user-item.js
+++ client/templates/users/user-item.js
@@ -1,15 +1,6 @@
 Template.userItem.helpers({
   user: function() {
-    console.log(FlowRouter.current().params);
-    console.log(Meteor.user().username);
-    var temp = Meteor.users.findOne({username: FlowRouter.current().params.username});
-    console.log(temp.username);
-    return temp;
+    var userVar = FlowRouter.getParam("username");
+    return Meteor.users.findOne({username: userVar});
   },
-
-  compareCurUser: function() {
-    var profileUser = Meteor.users.findOne({username: FlowRouter.current().params.username});
-    console.log("profile: " + profileUser.username + ", current: " + Meteor.user().username);
-    return (profileUser.username === Meteor.user().username);
-  }
 });
