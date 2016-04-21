df70da911f507cec552f0c5b56e388d5f6d96f5c
Casey Fischer <caseyfischer@Caseys-MacBook-Pro.local>
2016-04-20 23:56:44 -0400

    Render questions related to courses on course page based on presence of tag

--- client/templates/courses/course-item.js
+++ client/templates/courses/course-item.js
@@ -2,4 +2,13 @@ Template.courseItem.helpers({
   course: function() {
     return Courses.findOne({_id: FlowRouter.current().params._id});
   },
+
+  relatedQuestions: function() {
+    console.log("getting related questions..." + this.courseNum);
+    return Questions.find({tags: {$all: [this.courseNum] }});
+    //var course = Courses.findOne({_id: FlowRouter.current().params._id});
+    //console.log("Course " + course);
+    //return Questions.find({tags: {$all: [course.courseNum]}});
+    // return Questions.find();
+  }
 });
