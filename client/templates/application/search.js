9efd33873a2e02437eae44a8f745fcaa41929da8
Casey Fischer <caseyfischer@Caseys-MBP.wv.cc.cmu.edu>
2016-04-23 22:30:50 -0400

    Start over search

--- client/templates/application/search.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.isClient) {
-  Template.search.events({
-    'submit .search' (event) {
-      event.preventDefault();
-      Session.set('searchValue', event.target.search.value);
-      console.log("initiating search for " + event.target.search.value);
-    }
-  });
-}
